cf1db4274732fe79f1268ad59309f7f681363d68
msessini <mario.sessini@etu.unistra.fr>
2022-02-02 22:33:58 +0100

    02/02

--- ControlScripts/result.sh
+++
@@ -1,3 +0,0 @@
- qsub -q    -o  -e  -F "$1 $2" Set_.sh
- qsub -q  \${que}  -o \${output} -e \${error} -F "$1 $2" Set_.sh
- qsub -q  ${que}  -o ${output} -e ${error} -F "$1 $2" Set_.sh
--- ControlScripts/test.pl
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/perl
-#
-SUBDIRS += CommonUtils/TauPolSoftware/TauDecaysInterface/
-system(sprintf("echo \"sed -i 's;SUBDIRS += TauSpiner/ CommonUtils/TauPolSoftware/TauDecaysInterface/;SUBDIRS += CommonUtils/TauPolSoftware/TauDecaysInterface/;g' Makefile\""));
